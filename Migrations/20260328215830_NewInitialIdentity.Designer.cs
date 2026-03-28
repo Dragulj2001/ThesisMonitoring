15079df568d7be8ccf75642f851a6457719d0444
Nikola Dragoljević <dragoljevic.ra30.2019@uns.ac.rs>
2026-03-28 22:58:41 +0100

    Definitivno resetovane migracije za Render

--- Migrations/20260328215830_NewInitialIdentity.Designer.cs
+++ Migrations/20260328215830_NewInitialIdentity.Designer.cs
@@ -2,18 +2,21 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using ThesisWebApp.Data;
 
 #nullable disable
 
-namespace ThesisWebApp.Migrations
+namespace ThesisWebApp.Migrations.ApplicationIdentityDb
 {
     [DbContext(typeof(ApplicationIdentityDbContext))]
-    partial class ApplicationIdentityDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260328215830_NewInitialIdentity")]
+    partial class NewInitialIdentity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20260328215830_NewInitialIdentity.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ThesisWebApp.Migrations.ApplicationIdentityDb
+{
+    /// <inheritdoc />
+    public partial class NewInitialIdentity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
